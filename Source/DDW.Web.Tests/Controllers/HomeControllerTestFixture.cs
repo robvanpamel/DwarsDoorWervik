69814d0f232af04cdcf2ab80286e385304183529
Rob Van Pamel <Rob>
2012-10-10 19:55:59 +0200

    referenced Nunit and added HomeControllertestfixture

---
+++ Source/DDW.Web.Tests/Controllers/HomeControllerTestFixture.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using DDW.Web.Controllers;
+using NUnit.Framework;
+
+namespace DDW.Web.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTestFixture
+    {
+        [Test]
+        [SetUICulture("nl-BE")]
+        public void WhenUICultureNlWebsiteIsShownInNl()
+        {
+            var controller = new HomeController();
+            var result = controller.Index();
+
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+    }
+}
