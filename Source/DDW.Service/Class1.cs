fe7bbc32ecad7d208c00694368f98568772bf365
Rob Van Pamel <Rob>
2012-10-10 18:43:11 +0200

    Create Project

---
+++ Source/DDW.Service/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DDW.Service
+{
+    public class LogonService
+    {
+        public void Register()
+        {
+            
+        }
+
+        public void Logon()
+        {
+            
+        }
+    }
+}
