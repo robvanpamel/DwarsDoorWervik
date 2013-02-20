633c83740545558b5e418de770c8e36ecad3e153
Rob Van Pamel <Rob>
2013-02-20 20:52:00 +0100

    added canvas tests

---
+++ Source/DDW.Web/Controllers/Html5TryoutsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DDW.Web.Controllers
+{
+    public class Html5TryoutsController : Controller
+    {
+        //
+        // GET: /Html5Tryouts/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Canvas()
+        {
+            return View();
+        }
+
+    }
+}
